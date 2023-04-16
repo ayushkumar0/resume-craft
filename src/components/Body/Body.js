f00bab25a4b6281be93bf713b84eef2e753168a6
Ayush Kumar <akayushkumar03@gmail.com>
2023-04-16 21:39:24 +0530

    Body component completed

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,19 +1,25 @@
 import React from "react";
+import { ArrowDown } from "react-feather";
+
 import styles from "./Body.module.css";
 function Body() {
-  const colors = {"239ce2","#48bb78","#0bc5ea","#a0aec0","#ed8936"};
+  const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
   return (
     <div className={styles.container}>
       <p className={styles.heading}>Rsume Craft</p>
       <div className={styles.toolbar}>
         <div className={styles.colors}>
-          <span className={styles.color} />
-          <span className={styles.color} />
-          <span className={styles.color} />
-          <span className={styles.color} />
-          <span className={styles.color} />
+          {colors.map((item) => (
+            <span
+              key={item}
+              style={{ backgroundColor: item }}
+              className={styles.color}
+            />
+          ))}
         </div>
-        <button>Download</button>
+        <button>
+          Download <ArrowDown />
+        </button>
       </div>
     </div>
   );
