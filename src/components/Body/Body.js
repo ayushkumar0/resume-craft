397e9423865b8bd25dd1b17af883950c9a5812d6
Ayush Kumar <akayushkumar03@gmail.com>
2023-06-25 14:22:23 +0530

    FInal

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,8 +1,12 @@
-import React from "react";
-import Editor from "../Editor/Editor";
+import React, { useRef, useState } from "react";
+import ReactToPrint from "react-to-print";
 import { ArrowDown } from "react-feather";
 
+import Editor from "../Editor/Editor";
+import Resume from "../Resume/Resume";
+
 import styles from "./Body.module.css";
+
 function Body() {
   const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
   const sections = {
@@ -14,6 +18,46 @@ function Body() {
     summary: "Summary",
     other: "Other",
   };
+  const resumeRef = useRef();
+
+  const [activeColor, setActiveColor] = useState(colors[0]);
+  const [resumeInformation, setResumeInformation] = useState({
+    [sections.basicInfo]: {
+      id: sections.basicInfo,
+      sectionTitle: sections.basicInfo,
+      detail: {},
+    },
+    [sections.workExp]: {
+      id: sections.workExp,
+      sectionTitle: sections.workExp,
+      details: [],
+    },
+    [sections.project]: {
+      id: sections.project,
+      sectionTitle: sections.project,
+      details: [],
+    },
+    [sections.education]: {
+      id: sections.education,
+      sectionTitle: sections.education,
+      details: [],
+    },
+    [sections.achievement]: {
+      id: sections.achievement,
+      sectionTitle: sections.achievement,
+      points: [],
+    },
+    [sections.summary]: {
+      id: sections.summary,
+      sectionTitle: sections.summary,
+      detail: "",
+    },
+    [sections.other]: {
+      id: sections.other,
+      sectionTitle: sections.other,
+      detail: "",
+    },
+  });
 
   return (
     <div className={styles.container}>
@@ -24,16 +68,36 @@ function Body() {
             <span
               key={item}
               style={{ backgroundColor: item }}
-              className={styles.color}
+              className={`${styles.color} ${
+                activeColor === item ? styles.active : ""
+              }`}
+              onClick={() => setActiveColor(item)}
             />
           ))}
         </div>
-        <button>
-          Download <ArrowDown />
-        </button>
+        <ReactToPrint
+          trigger={() => {
+            return (
+              <button>
+                Download <ArrowDown />
+              </button>
+            );
+          }}
+          content={() => resumeRef.current}
+        />
       </div>
       <div className={styles.main}>
-        <Editor sections={sections} />
+        <Editor
+          sections={sections}
+          information={resumeInformation}
+          setInformation={setResumeInformation}
+        />
+        <Resume
+          ref={resumeRef}
+          sections={sections}
+          information={resumeInformation}
+          activeColor={activeColor}
+        />
       </div>
     </div>
   );
